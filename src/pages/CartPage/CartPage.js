29a8fb0c3c471a9e5dc645bedfe17cc275343416
Avineet Bhat <surajbhat316@gmail.com>
2023-12-20 16:31:01 +0530

    Added Cart Increase and decrease functionality

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -16,6 +16,91 @@ export default function CartPage() {
 
     getCartItems();
   },[]);
+
+
+  async function handleIncrease(e , id, name, unitPrice, product_id){
+
+    let res = await fetch("http://localhost:8000/cart/addItem", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    id:product_id,
+                    price: unitPrice,
+                    name: name
+                }),
+                mode: "cors"
+            });
+
+    let data = await res.json();
+    console.log(data)
+
+    let updatedCart = cart.map((item) =>{
+      if(item._id === id){
+        item.quantity = item.quantity + 1;
+        item.price = item.quantity * item.unitPrice;
+      }
+      return item
+    })
+
+    setCart([...updatedCart])
+  }
+
+  async function handleDecrease(e , id, product_id, unitPrice){
+
+    let res = await fetch("http://localhost:8000/cart/deleteItem", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({
+                    id:product_id,
+                    price: unitPrice,
+                }),
+                mode: "cors"
+            });
+
+    let data = await res.json();
+    console.log(data)
+
+
+
+
+
+    let updatedCart = cart.map((item) =>{
+      if(item._id === id){
+
+        if(item.quantity === 1){
+          return null;
+        }
+        else{
+          item.quantity = item.quantity - 1;
+          item.price = item.quantity * item.unitPrice;
+        }
+      }
+      return item
+    })
+    console.log(updatedCart)
+    let newUpdatedList = removeNulls(updatedCart);
+    setCart([...newUpdatedList])
+  }
+
+  function removeNulls(updatedCart){
+    let newUpdatedList = [];
+    for(let i=0;i<updatedCart.length;i++){
+      if(updatedCart[i] !== null){
+        newUpdatedList.push(updatedCart[i])
+      }
+    }
+    return newUpdatedList;
+  }
+
+
   return (
     <div id='cartContainer'>
       {cart.map((item) => {
@@ -29,8 +114,8 @@ export default function CartPage() {
               <p><b>Quantity </b>{item.quantity}</p>
             </div>
             <div className='btn_group'>
-              <button className='btn btn-primary'>+</button>
-              <button className='btn btn-secondary'>-</button>
+              <button onClick={(e) => handleIncrease(e, item._id, item.name, item.unitPrice, item.product_id)} className='btn btn-primary'>+</button>
+              <button onClick={(e) => handleDecrease(e, item._id, item.product_id, item.unitPrice)} className='btn btn-secondary'>-</button>
             </div>
           </div>
         )
