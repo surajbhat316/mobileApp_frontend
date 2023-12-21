0401cb2045d4b25a24d8c558e79d110f4337e191
Avineet Bhat <surajbhat316@gmail.com>
2023-12-21 15:19:42 +0530

    Created Orders Page UI

--- src/pages/OrdersPage/OrdersPage.js
+++ src/pages/OrdersPage/OrdersPage.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./OrdersPage.css"
 
 export default function OrdersPage() {
+
+
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() =>{
+    async function getAllOrders(){
+      let res = await fetch("http://localhost:8000/order/");
+      let data = await res.json();
+      console.log(data)
+
+      setOrders([...data.orders]);
+    }
+
+    getAllOrders();
+  },[]);
+
   return (
-    <div>OrdersPage</div>
+    <div id='ordersContainer'>
+      {orders.map((item) => {
+
+        return (
+          <div className='orderItem' key={item._id}>
+            <div className='orderItemName'>
+              <p>{item.name}</p>
+            </div>
+            <div className='orderItemDetails'>
+              <p> <b>Total Price </b>{item.price}</p>
+              <p><b>Quantity </b>{item.quantity}</p>
+            </div>
+          </div>
+        )
+
+      })}
+    </div>
   )
 }
